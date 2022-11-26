a3dcae23c6b13c33319e4c45140c3fe8d959e492
Omar Cornelio Munguia <omar.dev.clau@gmail.com>
2022-11-25 18:56:41 -0600

    first commit

---
+++ src/api/user/user.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateUserDto } from './user.dto';
+import { User } from './user.entity';
+
+@Injectable()
+export class UserService {
+  @InjectRepository(User)
+  private readonly repository: Repository<User>;
+
+  public getUsers(): Promise<User[]> {
+    return this.repository.find({ select: { name: true, email: true } });
+  }
+
+  public createUser(body: CreateUserDto): Promise<User> {
+    const user: User = new User();
+
+    user.name = body.name;
+    user.email = body.email;
+
+    return this.repository.save(user);
+  }
+}
